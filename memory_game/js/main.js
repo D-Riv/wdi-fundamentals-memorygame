4a4d7c356f871701f413d3e9c9576458b7af895b
D-Riv <41599276+D-Riv@users.noreply.github.com>
2020-01-13 17:05:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,25 @@
-let cards = ["queen", "queen", "king", "king"];
+console.log ("Up and running");
 
+let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
+function checkForMatch() {
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay === 2) {
-    
-}else if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-}else {
-    alert("Sorry, try again");
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
 }
+function flipCard(cardId) {
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
-
-
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+}
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+flipCard(0);
+flipCard(2);
+checkForMatch();
 
